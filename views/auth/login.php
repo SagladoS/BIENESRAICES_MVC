3fdddb48f82a7efc98a8a5a94f120b36080dc7a9
Salgado <santi80c14@hotmail.com>
2022-05-19 05:02:12 -0500

    Deployment Bienes Raices

---
+++ views/auth/login.php
@@ -0,0 +1,21 @@
+<main class="contenedor seccion contenido-centrado">
+    <h1 data-cy="heading-login">Iniciar Sesión</h1>
+
+    <?php foreach($errores as $error): ?>
+        <div data-cy="alerta-login" class="alerta error"><?php echo $error; ?></div>
+    <?php endforeach; ?>
+
+    <form data-cy="formulario-login" method="POST" class="formulario" novalidate>
+        <fieldset>
+            <legend>Email y Password</legend>
+
+            <label for="email">E-mail</label>
+            <input data-cy="login-email" type="email" name="email" placeholder="Tu Email" id="email">
+
+            <label for="password">Password</label>
+            <input data-cy="login-password" type="password" name="password" placeholder="Tu Password" id="password">
+        </fieldset>
+    
+        <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
+    </form>
+</main>
---
+++ views/paginas/propiedades.php
@@ -0,0 +1,5 @@
+<main class="contenedor seccion">
+
+<h2 data-cy="heading-propiedades">Casas y Depas en Venta</h2>
+    <?php include 'listado.php'; ?>
+</main>
